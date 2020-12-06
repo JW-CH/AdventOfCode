6c849195b5052893a64f17eb38cb3a511dc5a044
JW-CH <17313367+JW-CH@users.noreply.github.com>
2020-12-06 10:09:49 +0100

    AoC day 6

---
+++ AdventOfCode/Challenges/2020/Day_6.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Challenges._2020
+{
+    class Day_6 : AChallenge2020
+    {
+        public override string PartOne()
+        {
+            var answers = GetAnswersCombined();
+
+            var count = answers.Select(k => k.Count).Sum();
+
+            return $"Answers count: {count}";
+        }
+
+        public override string PartTwo()
+        {
+            var groupAnswers = GetAnswers();
+
+            var list = new List<int>();
+
+            foreach (var group in groupAnswers)
+            {
+                var counter = 0;
+                var firstItem = group[0];
+
+                foreach (var c in firstItem)
+                {
+                    var itemInAllGroups = true;
+                    for (int i = 1; i < group.Count; i++)
+                    {
+                        if (!group[i].Contains(c))
+                        {
+                            itemInAllGroups = false;
+                            break;
+                        }
+                    }
+
+                    if (itemInAllGroups)
+                    {
+                        counter++;
+                    }
+                }
+
+                list.Add(counter);
+            }
+
+            return $"Result: {list.Sum()}";
+        }
+
+        public IEnumerable<HashSet<char>> GetAnswersCombined()
+        {
+            var groupAnswer = InputText.Split("\r\n\r\n").ToList();
+
+            var hashSet = groupAnswer.Select(k => new HashSet<char>(k.Replace("\r\n", "").ToCharArray()));
+
+            return hashSet;
+        }
+
+        public IEnumerable<List<char[]>> GetAnswers()
+        {
+            var groupAnswer = InputText.Split("\r\n\r\n").ToList();
+
+            var hashSet = groupAnswer.Select(k => k.Split("\r\n").Select(k=>k.ToCharArray()).ToList());
+
+            return hashSet;
+        }
+    }
+}
