c3740d4df4a09c7afb0473d15d185a7865cfd65f
JW-CH <17313367+JW-CH@users.noreply.github.com>
2020-12-16 18:46:10 +0100

    AoC day 11 & 12 (12 only part 1 correct)

---
+++ AdventOfCode/Challenges/2020/Day_11.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Challenges._2020
+{
+    class Day_11 : AChallenge2020
+    {
+        public override string PartOne()
+        {
+            var input = GetInput();
+
+            Work(input, 4, true);
+
+            return $"{string.Join(Environment.NewLine, input.Select(k => string.Join("", k))).Count(k => k == '#')}";
+        }
+
+        private void Work(char[][] input, int occupiedSeatsCriteria, bool partOne)
+        {
+            var hasChanges = true;
+
+            while (hasChanges)
+            {
+                hasChanges = false;
+
+                var inputCopy = input.Select(a => a.ToArray()).ToArray();
+
+                for (int x = 0; x < inputCopy.Length; x++)
+                {
+                    for (int y = 0; y < inputCopy[x].Length; y++)
+                    {
+                        if (inputCopy[x][y] == '.')
+                        {
+                            continue;
+                        }
+
+                        var occupied = GetOccupiedSeats(inputCopy, x, y, partOne);
+
+                        var place = input[x][y];
+
+                        if (occupied == 0)
+                        {
+                            if (place != '#')
+                            {
+                                input[x][y] = '#';
+                                hasChanges = true;
+                            }
+                        }
+                        else if (occupied >= occupiedSeatsCriteria)
+                        {
+                            if (place != 'L')
+                            {
+                                input[x][y] = 'L';
+                                hasChanges = true;
+                            }
+                        }
+                    }
+                }
+                //Console.WriteLine(string.Join(Environment.NewLine, input.Select(k => string.Join("", k))));
+                //Console.WriteLine();
+            }
+        }
+
+        public override string PartTwo()
+        {
+            var input = GetInput();
+
+            Work(input, 5, false);
+
+            //return string.Join(Environment.NewLine, input.Select(k=>string.Join("", k)));
+            return $"{string.Join(Environment.NewLine, input.Select(k => string.Join("", k))).Count(k => k == '#')}";
+        }
+
+        public char[][] GetInput()
+        {
+            return Input.Select(k => k.ToCharArray()).ToArray();
+        }
+
+        private int GetOccupiedSeats(char[][] input, int x, int y, bool partOne)
+        {
+            var directions = new[] { (0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1) };
+            var occupied = 0;
+
+            foreach (var direction in directions)
+            {
+                var newX = x + direction.Item1;
+                var newY = y + direction.Item2;
+                if (PlaceExists(input, newX, newY))
+                {
+                    var place = input[newX][newY];
+
+                    if (place == '#')
+                    {
+                        occupied++;
+                    }
+                    else if (place == '.')
+                    {
+                        if (partOne)
+                        {
+                            continue;
+                        }
+                        while (true)
+                        {
+                            newX = newX + direction.Item1;
+                            newY = newY + direction.Item2;
+
+                            if (PlaceExists(input, newX, newY))
+                            {
+                                if (input[newX][newY] != '.')
+                                {
+                                    if (input[newX][newY] == '#')
+                                    {
+                                        occupied++;
+                                    }
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return occupied;
+        }
+
+        private bool PlaceExists(char[][] input, int x, int y)
+        {
+            return (x >= 0 && x < input.Length) && (y >= 0 && y < input[0].Length);
+        }
+    }
+}
---
+++ AdventOfCode/Challenges/2020/Day_12.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Challenges._2020
+{
+    class Day_12 : AChallenge2020
+    {
+        public override string PartOne()
+        {
+            (int ns, int ew, int deg) coords = (0, 0, 90);
+
+            foreach (var action in GetInput())
+            {
+                coords = Ns(action, coords.ns, coords.ew, coords.deg, true);
+            }
+
+            return $"{(coords.ns > 0 ? $"North: {coords.ns}" : $"South: {coords.ns * -1}")}, {(coords.ew > 0 ? $"East: {coords.ew}" : $"West: {coords.ew * -1}")} -> {Math.Abs(coords.ns) + Math.Abs(coords.ew)}";
+        }
+
+        private static (int ns, int ew, int deg) Ns((char cardinalDirection, int num) action, int ns, int ew, int deg, bool part1)
+        {
+            switch (action.cardinalDirection)
+            {
+                case 'N':
+                    ns += action.num;
+                    break;
+                case 'E':
+                    ew += action.num;
+                    break;
+                case 'S':
+                    ns -= action.num;
+                    break;
+                case 'W':
+                    ew -= action.num;
+                    break;
+                case 'L':
+                    deg = (deg - action.num + 360) % 360;
+                    break;
+                case 'R':
+                    deg = (deg + action.num) % 360;
+                    break;
+                case 'F':
+                    if (part1)
+                    {
+                        switch (deg)
+                        {
+                            case 0:
+                                ns += action.num;
+                                break;
+                            case 90:
+                                ew += action.num;
+                                break;
+                            case 180:
+                                ns -= action.num;
+                                break;
+                            case 270:
+                                ew -= action.num;
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            return (ns, ew, deg);
+        }
+
+        public override string PartTwo()
+        {
+            (int ns, int ew, int deg) coords = (1, 10, 90);
+
+            var posX = 0;
+            var posY = 0;
+
+            foreach (var action in GetInput())
+            {
+                coords = Ns(action, coords.ns, coords.ew, coords.deg, false);
+
+                if (action.cardinalDirection == 'F')
+                {
+                    posX += (coords.ns * action.num);
+                    posY += (coords.ew * action.num);
+                }
+            }
+
+            return $"{(posX > 0 ? $"North: {posX}" : $"South: {posX * -1}")}, {(posY > 0 ? $"East: {posY}" : $"West: {posY * -1}")} -> {Math.Abs(posX) + Math.Abs(posY)}";
+        }
+
+        public List<(char cardinalDirection, int num)> GetInput()
+        {
+            return Input.Select(k => (cardinalDirection: k.Substring(0, 1).ToCharArray()[0], num: Convert.ToInt32(k.Substring(1, k.Length - 1)))).ToList();
+        }
+    }
+}
