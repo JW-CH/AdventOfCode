9a1454233efaf399547a3e375ad453f99ed5d32d
JW-CH <17313367+JW-CH@users.noreply.github.com>
2021-12-02 08:34:33 +0100

    AoC Day 2

--- AdventOfCode/Challenges/2021/Challenges.cs
+++ AdventOfCode/Challenges/2021/Challenges.cs
@@ -51,12 +51,58 @@ namespace AdventOfCode.Challenges._2021
     {
         public override string PartOne()
         {
-            throw new NotImplementedException();
+            int depth = 0;
+            int horizontalPos = 0;
+
+            foreach (var item in Input.Select(i => i.Split(' ')).Select(i => new { direction = i[0], value = Convert.ToInt32(i[1]) }))
+            {
+                switch (item.direction)
+                {
+                    case "forward":
+                        horizontalPos+=item.value;
+                        break;
+                    case "up":
+                        depth -= item.value;
+                        break;
+                    case "down":
+                        depth += item.value;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                        break;
+                }
+            }
+
+            return $"Multiply {horizontalPos}*{depth}, ({nameof(horizontalPos)}*{nameof(depth)}): {horizontalPos*depth}";
         }
 
         public override string PartTwo()
         {
-            throw new NotImplementedException();
+            int aim = 0;
+            int depth = 0;
+            int horizontalPos = 0;
+
+            foreach(var item in Input.Select(i=>i.Split(' ')).Select(i=>new {direction = i[0], value = Convert.ToInt32(i[1])}))
+            {
+                switch (item.direction)
+                {
+                    case "forward":
+                        horizontalPos += item.value;
+                        depth += aim * item.value;
+                        break;
+                    case "up":
+                        aim -= item.value;
+                        break;
+                    case "down":
+                        aim += item.value;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                        break;
+                }
+            }
+
+            return $"Multiply {horizontalPos}*{depth}, ({nameof(horizontalPos)}*{nameof(depth)}): {horizontalPos * depth}";
         }
     }
 
