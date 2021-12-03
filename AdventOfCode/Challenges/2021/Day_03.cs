0715bba240df4bd8e4492f128b22c39b5649d0b5
JW-CH <17313367+JW-CH@users.noreply.github.com>
2021-12-03 11:22:59 +0100

    Aoc Day 3 Part 2

--- AdventOfCode/Challenges/2021/Day_03.cs
+++ AdventOfCode/Challenges/2021/Day_03.cs
@@ -10,14 +10,14 @@ namespace AdventOfCode.Challenges._2021
         {
             var list = Input.Select(i => i.ToCharArray()).ToArray();
             var gamma = "";
-            for (int i = 0; i < Input.First().ToCharArray().Length; i++)
+            for (int i = 0; i < list.First().Length; i++)
             {
                 var count0 = 0;
                 var count1 = 0;
 
                 for (int j = 0; j < Input.Count; j++)
                 {
-                    if(list[j][i] == '0')
+                    if (list[j][i] == '0')
                         count0++;
                     else
                         count1++;
@@ -27,12 +27,68 @@ namespace AdventOfCode.Challenges._2021
 
             var epsilon = BitWiseComplement(gamma);
 
-            return $"{Convert.ToInt32(gamma, 2)*Convert.ToInt32(epsilon, 2)}";
+            return $"{Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2)}";
         }
 
         public override string PartTwo()
         {
-            throw new NotImplementedException();
+            var list = Input.Select(i => i.ToCharArray()).ToArray();
+
+            var oxygenList = list;
+            var co2List = list;
+            var skip1 = false;
+            var skip2 = false;
+
+            for (int i = 0; i < list.First().Length; i++)
+            {
+                if (!skip1)
+                {
+                    var bit = GetMostCommonBit(oxygenList, i);
+
+                    oxygenList = oxygenList.Where(x => x[i] == bit).ToArray();
+                }
+
+                if (!skip2)
+                {
+                    var bit = GetMostCommonBit(co2List, i) == '1' ? '0' : '1';
+
+                    co2List = co2List.Where(x => x[i] == bit).ToArray();
+                }
+
+                skip1 = oxygenList.Length == 1;
+                skip2 = co2List.Length == 1;
+
+                if (skip2)
+                {
+                    var x = 1;
+                }
+
+                if (skip1 && skip2)
+                {
+                    break;
+                }
+            }
+
+            var oxygen = new string(oxygenList.First());
+            var co2 = new string(co2List.First());
+
+            return $"{Convert.ToInt32(oxygen, 2) * Convert.ToInt32(co2, 2)}";
+        }
+
+        private char GetMostCommonBit(char[][] input, int pos)
+        {
+            var count0 = 0;
+            var count1 = 0;
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (input[j][pos] == '0')
+                    count0++;
+                else
+                    count1++;
+            }
+
+            return count0 > count1 ? '0' : '1';
         }
 
         private string BitWiseComplement(string firstBinaryNumber)
